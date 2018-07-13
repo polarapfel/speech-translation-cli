05df330cc07b5563a135804b99546806758173aa
Tobias Weisserth <tobiaw@microsoft.com>
2018-07-13 12:52:21 -0700

    added logging

--- src/main/java/com/microsoft/speechtranslationclient/Client.java
+++ src/main/java/com/microsoft/speechtranslationclient/Client.java
@@ -1,8 +1,33 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Microsoft.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.microsoft.speechtranslationclient;
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.websocket.ClientEndpoint;
 import javax.websocket.CloseReason;
@@ -23,28 +48,38 @@ import javax.websocket.WebSocketContainer;
 
 @ClientEndpoint(configurator = Config.class)
 public class Client {
+    
+    private Logger classLogger = LogManager.getLogger(Client.class);
     static String host = "wss://dev.microsofttranslator.com";
     static String path = "/speech/translate";
     static String params = "?api-version=1.0&from=en-US&to=it-IT&features=texttospeech&voice=it-IT-Elsa";
     static String uri = host + path + params;
-
+    //static String uri = "wss://echo.websocket.org";
+    
     //static String input_path = "speak.wav";
-    static String input_path = "C:\\tmp\\speak.wav";
-    static String output_path = "C:\\tmp\\speak2.wav";
+    static String input_path = "/tmp/speak.wav";
+    static String output_path = "/tmp/speak2.wav";
 
     Session session = null;
 
     @OnOpen
     public void onOpen(Session session) {
+        if (session == null){
+            classLogger.error("onOpen session is null.");
+        } else{
+            classLogger.trace("OnOpen query string: " + session.getQueryString());
+            classLogger.trace("OnOpen protocol version: " + session.getProtocolVersion());
+            classLogger.trace("OnOpen id: " + session.getId());
+            classLogger.trace("OnOpen sub protocol: " + session.getNegotiatedSubprotocol());
+            classLogger.trace("OnOpen secure? " + session.isSecure());
+        }
         this.session = session;
-        System.out.println ("Connected.");
         SendAudio ();
     }
 
     @OnMessage
     public void onTextMessage(String message, Session session){
-        System.out.println ("Text message received.");
-        System.out.println (message);
+        classLogger.trace("OnMessage:String " + message);
     }
 
 /*
@@ -57,26 +92,26 @@ See:
 */
     @OnMessage
     public void onBinaryMessage(byte[] buffer, Session session) {
+        classLogger.trace("OnMessage:byte buffer " + buffer.length + "bytes received.");
         try {
-            System.out.println ("Binary message received.");
             FileOutputStream stream = new FileOutputStream(output_path);
             stream.write(buffer);
             stream.close();
-            System.out.println ("Message contents written to file.");
+            classLogger.trace("Received buffer written to file.");
             Close ();
         } catch (Exception e) {
-            e.printStackTrace ();
+            classLogger.error("Error in OnMessage: byte buffer", e);
         }
     }
 
     @OnError
     public void onError(Session session, Throwable e) {
-        System.out.println ("Error message received: " + e.getMessage ());
+        classLogger.error("OnError " + e.getMessage(), e);
     }
 
     @OnClose
     public void myOnClose (CloseReason reason) {
-        System.out.println ("Close message received: " + reason.getReasonPhrase());
+        classLogger.trace("OnClose " + reason.getReasonPhrase());
     }
 
     public void SendAudio() {
@@ -88,43 +123,43 @@ See:
             stream.close();
             sendMessage (buffer);
         } catch (Exception e) {
-            e.printStackTrace ();
+            classLogger.error("Error while sending audio.", e);
         }
     }
 
     public void sendMessage(byte[] message) {
         try {
-            System.out.println ("Sending audio.");
+            classLogger.trace("Sending audio.");
             OutputStream stream = this.session.getBasicRemote().getSendStream();
             stream.write (message);
 /* Make sure the audio file is followed by silence.
  * This lets the service know that the audio input is finished. */
-            System.out.println ("Sending silence.");
+            classLogger.trace("Sending silence.");
             byte silence[] = new byte[3200000];
             stream.write (silence);
             stream.flush();
         } catch (Exception e) {
-            e.printStackTrace ();
+            classLogger.error("Error while sending message.", e);
         }
     }
 
     public void Connect () throws Exception {
         try {
-            System.out.println("Connecting.");
+            classLogger.trace("Connecting.");
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
 /* Some code samples show container.connectToServer as returning a Session, but this seems to be false. */
             container.connectToServer(this, new URI(uri));
         } catch (Exception e) {
-            e.printStackTrace ();
+            classLogger.error("Error connecting.", e);
         }
     }
 
     public void Close () throws Exception {
         try {
-            System.out.println("Closing connection.");
+            classLogger.trace("Closing connection.");
             this.session.close ();
         } catch (Exception e) {
-            e.printStackTrace ();
+            classLogger.error("Error closing.", e);
         }
     }
 }
---
+++ src/test/java/com/microsoft/speechtranslationcli/SpeechTranslationCliTest.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Microsoft.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.microsoft.speechtranslationcli;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Assumptions.*;
+
+/**
+ *
+ * @author Tobias Weisserth <tobias.weisserth@microsoft.com>
+ */
+public class SpeechTranslationCliTest {
+    
+    public SpeechTranslationCliTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeAll
+    public static void setUp() {
+    }
+    
+    @AfterAll
+    public static void tearDown() {
+    }    
+}
